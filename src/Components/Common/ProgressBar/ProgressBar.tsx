bbe24e46a706dd372aefdce12642abd95e4540ed
Your Name <you@example.com>
2024-11-16 10:35:24 +0300

    refactor: ProgressBar and UpButton components (a style change logic)

--- src/Components/Common/ProgressBar/ProgressBar.tsx
+++ src/Components/Common/ProgressBar/ProgressBar.tsx
@@ -1,29 +1,32 @@
-import { type FC, type ReactElement, useEffect, useRef } from 'react';
+import { type FC, type ReactElement, useEffect, useState } from 'react';
+import { useThrottledCallback } from 'use-debounce';
 import classes from './ProgressBar.module.scss';
 import { EventType } from '../../../utils/types/enums';
 
 export const ProgressBar: FC = (): ReactElement => {
-  const progressBar = useRef<HTMLDivElement>(null);
+  const [scrollableWidth, setScrollableWidth] = useState<number>(0);
+
+  const styles = { width: `${scrollableWidth}%` };
 
   const onScroll = (): void => {
     const element: HTMLElement = document.documentElement;
 
     const scroll = element.scrollTop;
     const height = element.scrollHeight - element.clientHeight;
-    const scrolled = (scroll / height) * 100;
+    const scrollableValue = (scroll / height) * 100;
 
-    if (progressBar.current) {
-      progressBar.current.style.width = `${scrolled}%`;
-    }
+    setScrollableWidth(scrollableValue);
   };
 
+  const onThrottledScroll = useThrottledCallback(onScroll, 30);
+
   useEffect(() => {
-    window.addEventListener(EventType.Scroll, onScroll);
+    window.addEventListener(EventType.Scroll, onThrottledScroll);
 
     return () => {
-      window.removeEventListener(EventType.Scroll, onScroll);
+      window.removeEventListener(EventType.Scroll, onThrottledScroll);
     };
-  }, []);
+  }, [onThrottledScroll]);
 
-  return <div className={classes.progressBar} ref={progressBar} />;
+  return <div style={styles} className={classes.progressBar} />;
 };
